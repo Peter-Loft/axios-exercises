05667f9d4aea5f86c77ce0a40e615c0226fd497b
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-02-14 19:05:09 -0600

    began work for question 3 of part 2

--- async-demo.js
+++ async-demo.js
@@ -2,61 +2,85 @@
 
 const BASE_URL = "http://numbersapi.com/";
 const BASE_URL_CARDS = "http://deckofcardsapi.com/";
-const HEADERS = {'content-type': 'application/json'}
+const HEADERS = { 'content-type': 'application/json' }
 const $body = $('body');
 
-async function showFavNumTrivia(num) {
-  const response = await axios.get(`${BASE_URL}${num}/trivia?json`);
-  console.log(response.data.text);
-}
+
+// async function showFavNumTrivia(num) {
+//   const response = await axios.get(`${BASE_URL}${num}/trivia?json`);
+//   console.log(response.data.text);
+// }
 
 
-async function showFavNumsTrivia() {
-  const response = await axios.get(`${BASE_URL}14,1..3,27,45/trivia?json`);
-  // const response2 = await axios.get(`${BASE_URL}${27}/trivia?json`);
-  // const response3 = await axios.get(`${BASE_URL}${3}/trivia?json`);
-  // const response4 = await axios.get(`${BASE_URL}${99}/trivia?json`);
-  // const response5 = await axios.get(`${BASE_URL}${45}/trivia?json`);
-  console.log(response.data);
-  console.log(response.data['2']);
-  console.log(response.data['14']);
-  console.log(response.data['27']);
-  console.log(response.data['45']);
+// async function showFavNumsTrivia() {
+//   const response = await axios.get(`${BASE_URL}14,1..3,27,45/trivia?json`);
+//   // const response2 = await axios.get(`${BASE_URL}${27}/trivia?json`);
+//   // const response3 = await axios.get(`${BASE_URL}${3}/trivia?json`);
+//   // const response4 = await axios.get(`${BASE_URL}${99}/trivia?json`);
+//   // const response5 = await axios.get(`${BASE_URL}${45}/trivia?json`);
+//   console.log(response.data);
+//   console.log(response.data['2']);
+//   console.log(response.data['14']);
+//   console.log(response.data['27']);
+//   console.log(response.data['45']);
 
 
-}
+// }
 
 // showFavNumsTrivia();
 
-async function showFavNumTriviaFourTimes(num) {
-  const response1 =  axios.get(`${BASE_URL}${num}/trivia?json`);
-  const response2 =  axios.get(`${BASE_URL}${num}/trivia?json`);
-  const response3 =  axios.get(`${BASE_URL}${num}/trivia?json`);
-  const response4 =  axios.get(`${BASE_URL}${num}/trivia?json`);
+// async function showFavNumTriviaFourTimes(num) {
+//   const response1 =  axios.get(`${BASE_URL}${num}/trivia?json`);
+//   const response2 =  axios.get(`${BASE_URL}${num}/trivia?json`);
+//   const response3 =  axios.get(`${BASE_URL}${num}/trivia?json`);
+//   const response4 =  axios.get(`${BASE_URL}${num}/trivia?json`);
 
-  let responses = [await response1, await response2, await response3, await response4];
-  for(let response of responses){
-    console.log(response.data.text);
-  }
-  
-}
+//   let responses = [await response1, await response2, await response3, await response4];
+//   for(let response of responses){
+//     console.log(response.data.text);
+//   }
+
+// }
 
 // showFavNumTriviaFourTimes(10);
 
 // ************************** DECK OF CARDS API EXERCISE ****************************
 
-async function drawAndShuffleCard() {
-  const shuffleResponse = await axios.get(`${BASE_URL_CARDS}api/deck/new/shuffle/?deck_count=1`);
-  // const shuffleResponse2 = await axios.get(`${BASE_URL_CARDS}api/deck/new/draw/?count=1`);
-  const deckId = shuffleResponse.data.deck_id;
-  const drawCard = await axios.get(`${BASE_URL_CARDS}api/deck/${deckId}/draw/?count=1`);
-  console.log("shuffle response: ", drawCard.data.cards);
+// async function drawAndShuffleCard() {
+//   const shuffleResponse = await axios.get(`${BASE_URL_CARDS}api/deck/new/shuffle/?deck_count=1`);
+//   // const shuffleResponse2 = await axios.get(`${BASE_URL_CARDS}api/deck/new/draw/?count=1`);
+//   const deckId = shuffleResponse.data.deck_id;
+//   const drawCard = await axios.get(`${BASE_URL_CARDS}api/deck/${deckId}/draw/?count=1`);
+//   console.log("shuffle response: ", drawCard.data.cards);
+// }
+// drawAndShuffleCard();
+
+
+async function prepareDeck() {
+  let resp = await axios.get(`${BASE_URL_CARDS}api/deck/new/shuffle/?deck_count=1`);
+  let deckId = resp.data.deck_id;
+  return deckId;
 }
 
 
-drawAndShuffleCard();
+async function drawCard(deckId) {
+  const card = await axios.get(`${BASE_URL_CARDS}api/deck/${deckId}/draw/?count=1`);
 
 
+}
+
+
+async function setUp() {
+
+  $("#drawn-cards").empty();
+  // prepare deck
+  let deckId = await prepareDeck();
+  let url = `${BASE_URL_CARDS}api/deck/${deckId}/draw/?count=1`;
+  
+  
+}
+
+$body.on("sumbit", drawCard);
 
 
 // promise to be resolved (hopefully)
